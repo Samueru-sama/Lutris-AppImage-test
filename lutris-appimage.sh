a840782f16f12f60c430c4135b09224db18bd6c4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-06-27 08:44:02 -0400

    remove 32bit packages

--- lutris-appimage.sh
+++ lutris-appimage.sh
@@ -13,13 +13,9 @@ run_install() {
 	set -e
 
 	INSTALL_PKGS=(
-		lutris egl-wayland vulkan-radeon lib32-vulkan-radeon vulkan-tools
-		vulkan-intel lib32-vulkan-intel vulkan-nouveau lib32-vulkan-nouveau
-		lib32-libpipewire libpipewire pipewire lib32-libpipewire libpulse
-		lib32-libpulse vkd3d lib32-vkd3d wget xdg-utils vulkan-mesa-layers
-		lib32-vulkan-mesa-layers freetype2 lib32-freetype2 fuse2 mangohud
-		lib32-mangohud gamescope gamemode lib32-gamemode wine lib32-libglvnd
-		lib32-gnutls xterm python-protobuf xdg-desktop-portal-gtk pipewire-pulse zenity-gtk3 libtheora glew glfw
+		lutris egl-wayland vulkan-radeon vulkan-tools vulkan-intel vulkan-nouveau libpipewire pipewire 
+		libpulse vkd3d wget xdg-utils vulkan-mesa-layers freetype2 fuse2 mangohud gamescope gamemode 
+		wine xterm python-protobuf xdg-desktop-portal-gtk pipewire-pulse zenity-gtk3 libtheora glew glfw
 	)
 
 	echo '== checking for updates'
